dfd8edc191e008fbb3a57e3b316440cb950745de
bakibi <elkaissimath@gmail.com>
2018-02-25 19:57:26 +0000

    ajout de la methode fetch history

--- src/apiget/Application.java
+++ src/apiget/Application.java
@@ -20,42 +20,7 @@ public class Application {
 		//app.attack_one_time();
 		//app.attack_per_minute();
 		
-		String uri = "https://www.alphavantage.co/query?"
-				+ "function=TIME_SERIES_DAILY&"
-				+ "symbol="+"AAPL"+"&"
-						//+ "interval=&"
-								+ "outputsize=full&"
-								+ "apikey=BI8MLA7OT3J6LW18";
-		URL a;
-		try {
-			a = new URL(uri);
-			URLConnection yc = a.openConnection();
-			BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
-			String all = "";
-			String lu;
-			
-			while((lu = in.readLine())!=null)
-				all+=lu;
-			 try {
-				JSONObject obj = new JSONObject(all);
-				System.out.println("here we are");
-				JSONObject obj1 = obj.getJSONObject("Time Series (Daily)");
-				
-				Iterator<?> keys   =obj1.keys();
-				
-				
-				
-				//System.out.println(obj1);;
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		} catch ( IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+		
 	}
 
 }
--- src/apiget/RequestAlpha.java
+++ src/apiget/RequestAlpha.java
@@ -5,7 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Iterator;
 import java.util.Vector;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 
@@ -21,7 +25,7 @@ public class RequestAlpha  {
 			
 			URLConnection yc = a.openConnection();
 			BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
-	       
+			
 	        
 	        Point pt;
 	        read_metatdate(in);
@@ -41,6 +45,46 @@ public class RequestAlpha  {
 	
 	
 	
+	public static Vector<Point>requestHistrory(String Symbole){
+		Vector<Point> ans = new Vector<>();
+		String uri = "https://www.alphavantage.co/query?"
+				+ "function=TIME_SERIES_DAILY&"
+				+ "symbol="+"AAPL"+"&"
+						//+ "interval=&"
+								+ "outputsize=full&"
+								+ "apikey=BI8MLA7OT3J6LW18";
+		URL a;
+		try {
+			a = new URL(uri);
+			URLConnection yc = a.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+			String all = "";
+			String lu;
+			
+			while((lu = in.readLine())!=null)
+				all+=lu;
+			 try {
+				JSONObject obj = new JSONObject(all);
+				System.out.println("here we are");
+				JSONObject obj1 = obj.getJSONObject("Time Series (Daily)");
+				
+				Iterator<?> keys   =obj1.keys();
+				
+				
+				
+				//System.out.println(obj1);;
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} catch ( IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return ans;
+	}
 	
 	
 	
